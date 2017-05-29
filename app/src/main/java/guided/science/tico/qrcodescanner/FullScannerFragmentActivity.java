6087a183e15ab2733984cf0bf3417d07f14423f3
Vipul Garg <vipul.bigevil@gmail.com>
2017-05-29 15:30:34 +0530

    Initial commit

---
+++ app/src/main/java/guided/science/tico/qrcodescanner/FullScannerFragmentActivity.java
@@ -0,0 +1,12 @@
+package guided.science.tico.qrcodescanner;
+
+import android.os.Bundle;
+
+public class FullScannerFragmentActivity extends BaseScannerActivity {
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        setContentView(R.layout.activity_full_scanner_fragment);
+        setupToolbar();
+    }
+}
